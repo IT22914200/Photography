ffbed4fb493361729fd34ba7774c2720bea3c4f0
IT22371836 <it22371836@my.sliit.lk>
2025-05-16 00:41:28 +0530

    Update MediaDTO.java
    implement getters and setters

--- backend/src/main/java/backend/dtos/MediaDTO.java
+++ backend/src/main/java/backend/dtos/MediaDTO.java
@@ -20,5 +20,45 @@ public class MediaDTO {
         this.relatedPost = relatedPost;
     }
 
-    
+    // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isDeleteStatus() {
+        return deleteStatus;
+    }
+
+    public void setDeleteStatus(boolean deleteStatus) {
+        this.deleteStatus = deleteStatus;
+    }
+
+    public String getRelatedPost() {
+        return relatedPost;
+    }
+
+    public void setRelatedPost(String relatedPost) {
+        this.relatedPost = relatedPost;
+    }
 }
