3b40c548be3642546ffa0fdfaef2ebe0bda2c821
IT22371836 <it22371836@my.sliit.lk>
2025-05-16 00:25:40 +0530

    Update Media.java
    create getters and setters

--- backend/src/main/java/backend/model/Media.java
+++ backend/src/main/java/backend/model/Media.java
@@ -27,6 +27,46 @@ public class Media {
         this.deleteStatus = deleteStatus;
         this.relatedPost = relatedPost;
     }
-    
 
+    // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isDeleteStatus() {
+        return deleteStatus;
+    }
+
+    public void setDeleteStatus(boolean deleteStatus) {
+        this.deleteStatus = deleteStatus;
+    }
+
+    public PhotoPost getRelatedPost() {
+        return relatedPost;
+    }
+
+    public void setRelatedPost(PhotoPost relatedPost) {
+        this.relatedPost = relatedPost;
+    }
 }
