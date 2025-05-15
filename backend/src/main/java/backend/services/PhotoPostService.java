2b19c39ae642247ac61427a2135c9ade5c795f4e
IT22371836 <it22371836@my.sliit.lk>
2025-05-16 00:02:43 +0530

    Create PhotoPostService.java
    create get all posts and get posts by user

---
+++ backend/src/main/java/backend/services/PhotoPostService.java
@@ -0,0 +1,46 @@
+package com.example.photographsystem.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.photographsystem.models.PhotoPost;
+import com.example.photographsystem.models.User;
+import com.example.photographsystem.repositories.PhotoPostRepository;
+import com.example.photographsystem.repositories.UserRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PhotoPostService {
+
+    private final PhotoPostRepository cookingPostRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public PhotoPostService(PhotoPostRepository cookingPostRepository, UserRepository userRepository) {
+        this.cookingPostRepository = cookingPostRepository;
+        this.userRepository = userRepository;
+    }
+
+    // Get all posts
+    public List<PhotoPost> getAllPosts() {
+        return cookingPostRepository.findAllByDeleteStatusFalse();
+    }
+
+    // Get posts by user
+    public List<PhotoPost> getPostsByUser(String userId) {
+        return cookingPostRepository.findByCreatedByIdAndDeleteStatusFalse(userId);
+    }
+
+
+
+    
+
+    
+
+   
+
+   
+}
