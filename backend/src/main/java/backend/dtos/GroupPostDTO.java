77213ca3acc5dc34c4fb5d4219f4e0093304c49d
IT22371836 <it22371836@my.sliit.lk>
2025-05-17 00:31:40 +0530

    Update GroupPostDTO.java
    Add getters and setters

--- backend/src/main/java/backend/dtos/GroupPostDTO.java
+++ backend/src/main/java/backend/dtos/GroupPostDTO.java
@@ -164,5 +164,21 @@ public class GroupPostDTO {
             }
         }
 
-       
+        // Getters and setters
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
