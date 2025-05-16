05aa0175e7d5b69a0101f407da5cee5ab265af98
IT22371836 <it22371836@my.sliit.lk>
2025-05-17 00:36:35 +0530

    Create GroupPostService.java
    Set up to group post service

---
+++ backend/src/main/java/backend/services/GroupPostService.java
@@ -0,0 +1,59 @@
+package com.example.photographsystem.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.photographsystem.dtos.GroupPostDTO;
+import com.example.photographsystem.exceptions.ResourceNotFoundException;
+import com.example.photographsystem.models.Group;
+import com.example.photographsystem.models.GroupPost;
+import com.example.photographsystem.models.User;
+import com.example.photographsystem.repositories.GroupPostRepository;
+import com.example.photographsystem.repositories.GroupRepository;
+import com.example.photographsystem.repositories.UserRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GroupPostService {
+
+    private final GroupPostRepository groupPostRepository;
+    private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
+
+    @Autowired
+    public GroupPostService(GroupPostRepository groupPostRepository,
+                            UserRepository userRepository,
+                            GroupRepository groupRepository) {
+        this.groupPostRepository = groupPostRepository;
+        this.userRepository = userRepository;
+        this.groupRepository = groupRepository;
+    }
+
+    
+
+    
+
+    
+
+    
+     
+       
+       
+
+   
+        
+
+        
+       
+
+    
+        
+
+       
+
+    
+}
