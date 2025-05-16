88e388039a8e4eabb6ac4d224d0d46c9dbfd19af
IT22371836 <it22371836@my.sliit.lk>
2025-05-17 00:26:13 +0530

    Create GroupPostDTO.java
    create class for group post request

---
+++ backend/src/main/java/backend/dtos/GroupPostDTO.java
@@ -0,0 +1,27 @@
+package com.example.photographsystem.dtos;
+
+import java.util.Date;
+
+import com.example.photographsystem.models.Group;
+import com.example.photographsystem.models.User;
+
+public class GroupPostDTO {
+
+    public static class GroupPostRequest {
+        private String title;
+        private String description;
+        private String mediaUrl;
+        private String groupId;
+
+        
+
+   
+
+    
+
+        
+
+    
+
+       
+}
